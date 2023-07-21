5ac0795f93f78eb159a4c8dc509dacdff92c506e
JoshuaDuquette2 <joshuaduquette2@g.ucla.edu>
2023-07-21 04:11:36 -0700

    update

--- example1.py
+++ example1.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-import matplotlib as mpl
-mpl.use("agg")
 
 # -- Set page config
 apptitle = 'GW Quickview'
